e42e815b34b92d2d94092b59536d9c8353dbea40
Jeniffer Carvalho <jenicarvalho@live.com>
2023-04-11 20:02:46 -0300

    improved card styles

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,4 +1,5 @@
 import { styled } from "@/styles";
+import Link from "next/link"
 
 export const CardStyled = styled('div', {
   backgroundColor: '$white',
@@ -7,10 +8,13 @@ export const CardStyled = styled('div', {
   border: '1px solid #E6E7EB',
   boxShadow: '0px 10px 20px rgba(9, 0, 65, 0.05)',
   padding: 24,
+  textDecoration: 'none',
 
   footer: {
-    display: 'flex',
+    display: 'grid',
     justifyContent: 'space-between',
+    gridTemplateColumns: '1fr 1fr',
+    lineHeight: '30px',    
     color: '$darkGray',
 
     span: {
@@ -19,9 +23,53 @@ export const CardStyled = styled('div', {
   }
 });
 
-export const Breed = styled('h2', {
-  textTransform: 'uppercase',
+export const Breed = styled(Link, {
+  textTransform: 'uppercase', 
   fontSize: '$lg',
   color: '$black',
-  margin: '24px 0'
+  margin: '24px 0',
+  textDecoration: 'none',
+  fontWeight: 600,
+
+  display: 'flex',
+  justifyContent: 'space-between',
+
+  '&:hover': {
+    color: '$primary'
+  }
+});
+
+export const Overlay = styled(Link, {
+  position: 'relative',
+  borderRadius: 20,
+  overflow: 'hidden',
+
+  img: {
+    objectFit: 'cover'
+  },
+
+  variants: {
+    favorited: {
+      true: {
+        '&::before': {
+          content: '\\2665',
+          position: 'absolute',
+          backgroundColor: 'rgba(33, 33, 33, 0.5)',
+          width: '100%',
+          height: '99%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '$red',
+          fontSize: '$xl'
+        },               
+      },
+    },
+  }
+});
+
+export const Info = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
 });
