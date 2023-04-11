44fbd277ec06c8efe94c81f04064a2352130a2e3
Jeniffer Carvalho <jenicarvalho@live.com>
2023-04-11 20:04:10 -0300

    added getserversideprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,42 @@
-import Card from '@/components/Card'
-import List from '@/components/List'
-import Head from 'next/head'
+import axios from 'axios'
 
-export default function Home() {
+import Card from '@/components/Card';
+import Filter from '@/components/Filter';
+import Container from '@/components/Container';
+import Sort from '@/components/Sort';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+export default function Home({ cats }: any) {
+
   return (
     <>
       <Head>
         <title>Cat App</title>
       </Head>
-      <List>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </List>
+
+      <Container plain>
+        <Filter />
+        <Sort />
+      </Container>
+
+      <Container grid>
+        {cats && cats.map((cat: any) => (
+          <Card content={cat} key={cat.id} />
+        ))}
+      </Container>
     </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `has_breeds=1&order=asc&limit=20&api_key=${process.env.API_KEY}`;
+  const response = await axios.get(`${process.env.API_URL}${query}`);
+
+  return {
+    props: {
+      cats: response.data
+    }
+  }
+}
+
