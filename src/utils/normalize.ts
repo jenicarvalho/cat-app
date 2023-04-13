341de1b8b55297c14dad88ce17dd38e09ad2a28e
Jeniffer Carvalho <jenicarvalho@live.com>
2023-04-13 14:18:56 -0300

    normalize data from api

---
+++ src/utils/normalize.ts
@@ -0,0 +1,12 @@
+export const normalizeData = (data) => {
+  return data.map((item: any) => {
+    const [breed] = item.breeds;
+    return {
+      ...item,
+      breeds: {
+        ...breed,
+        weight: breed.weight.imperial
+      }
+    }
+  })
+}
