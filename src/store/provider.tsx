a74c7cb534c6ef17d271a4dc8fb7cc04500789f9
Jeniffer Carvalho <jenicarvalho@live.com>
2023-04-13 22:07:24 -0300

    fix store issues

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import { store } from '.';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import catsReducer from './features/cats/slice';
+
+const store = configureStore({
+  reducer: {
+    cats: catsReducer
+  }
+})
+
 
 export function Providers({ children }: any) {
   return (
@@ -10,3 +18,12 @@ export function Providers({ children }: any) {
     </Provider>
   )
 }
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
